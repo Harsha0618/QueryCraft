f76605e0e46bb642df8d9a8abddf92a6358a9b26
Yathapu HarshaVardhan reddy <yharshavardhan.2006@gmail.com>
2026-03-18 13:28:13 +0530

    Use environment variable for API URL

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import axios from 'axios'
 
-const API = 'http://localhost:8000'
+const API = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
 const useStore = create((set, get) => ({
   // Dataset state
